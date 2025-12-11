2e509f605a0b7e92b7c36f69636ed8e597cf3bef
Palaciinn <angel.palacin71@gmail.com>
2025-12-11 01:32:01 +0100

    Countdown 1.12.12 (Fix: login.html)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "countdown-v6.7"; // súbelo a v4, v5... cuando saques versión nueva
+const CACHE_NAME = "countdown-v6.8"; // súbelo a v4, v5... cuando saques versión nueva
 
 // Archivos esenciales que queremos cachear
 const ASSETS_TO_CACHE = [
